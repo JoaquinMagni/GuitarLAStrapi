d6d67bcab264746cbb79a9f7f792683e89cb62b4
Joaquin <joaquinagueromagni@gmail.com>
2023-07-11 21:18:03 -0300

    first commit

---
+++ src/components/ComponenteReact.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+export default function ComponenteReact() {
+    const [cliente, setCliente] = useState('Juan')
+
+    console.log('Cliente y Servidor')
+  return (
+    <div>{cliente}</div>
+  )
+}
